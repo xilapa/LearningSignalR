54e12e90f3cc1be8cf4b847d5f751304113fea6c
xilapa <junior.dirceu@pm.me>
2023-04-13 18:03:04 -0300

    signalR console client

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+var url = "http://localhost:5069/learning-hub";
+
+var conn = new HubConnectionBuilder()
+                            .WithUrl(url)
+                            .Build();
+                            
+conn.On<string>("ReceiveMessage", msg =>
+{
+    Console.WriteLine(msg);
+});
+
+try
+{
+    await conn.StartAsync();
+    Console.WriteLine("Connected to SignalR Server");
+
+    while (true)
+    {
+        var msg = string.Empty;
+
+        Console.WriteLine("Please specify the action:");
+        Console.WriteLine("0 - broadcast to all");
+        Console.WriteLine("exit - Exit the program");
+        
+        var action = Console.ReadLine();
+        if (action == "exit")
+            break;
+        
+        Console.WriteLine("Please specify the message:");
+        msg = Console.ReadLine();
+        
+        await conn.SendAsync("BroadcastMessage", msg);
+    }
+}catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.ReadKey();
+}
